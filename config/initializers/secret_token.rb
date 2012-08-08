35e73059f7f064eb78bc6a75957fd845958eb07f
Caleb <contact@calebtutty.com>
2012-08-08 20:17:20 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aucklandrails::Application.config.secret_token = 'b5b363eba1521305262f84a29d4cd7e0a36da68a664745f8ebaee451ad66940303a1ca371d19a1563999a28d582c23bd94d08a3ccd4ff5a3da99ac23a25e4304'
